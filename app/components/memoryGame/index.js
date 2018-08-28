a626a1fac88cb88690379857b5331ad3bb8beadc
Michael Butters <michaelbutters@gmail.com>
2018-08-28 21:17:33 +1200

    Button click logic on a single button

--- app/components/memoryGame/index.js
+++ app/components/memoryGame/index.js
@@ -22,7 +22,7 @@ export class MemoryGame extends RkComponent {
     super(props);
 
     this.state = {
-      done: false
+      text0: ''
     }
   }
 
@@ -35,12 +35,23 @@ export class MemoryGame extends RkComponent {
       });
     }
 
+    let handlePress0 = () => {
+      this.setState((prevState, props) => {
+        if(prevState.text0 === ''){
+          return { text0: 'kanji' }
+        }
+        else {
+          return { text0: '' }
+        }
+      });
+    }
+
 
     return (
       <View style={container}>
         <View rkType='memorygame'>
-          <RkButton rkType='memorycard basic square' onPress={handlePress}>
-            <RkText rkType='awesome hintColor' style={icon}>{FontAwesome.smile}</RkText>
+          <RkButton rkType='memorycard basic square' onPress={handlePress0}>
+            <RkText rkType='awesome hintColor'>{ this.state.text0 === '' ? FontAwesome.smile : this.state.text0 }</RkText>
           </RkButton>
           <RkButton rkType='memorycard basic square' onPress={handlePress}>
             <RkText rkType='awesome hintColor' style={icon}>{FontAwesome.smile}</RkText>
